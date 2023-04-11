eed8ebc665df20db7af3f274f94f6333a9839dd1
Geonmo <rjsah8671@naver.com>
2023-04-11 13:23:01 +0900

    init

---
+++ src/Books.java
@@ -0,0 +1,15 @@
+
+public class Books {
+    String title;
+    String id;
+    String date;
+    
+    public Books() {
+    }
+    
+    public Books(String title, String id, String date) {
+        this.title = title;
+        this.id = id;
+        this.date = date;
+    }
+}
--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,91 +1,68 @@
 import java.util.Scanner;
 
 public class MenuManager {
-	public static void main(String[] args) {
-		int num =5;
-		Scanner input = new Scanner(System.in);
-		
-		while (num != 6) {
-			System.out.println("1. Add Books");
-			System.out.println("2. Delete Books");
-			System.out.println("3. Edit Books");
-			System.out.println("4. View Books");
-			System.out.println("5. Show a Menu");
-			System.out.println("6. Exit");
-			System.out.println("Sellect one number between 1-6:");
-			num = input.nextInt();
-			if (num == 1) {
-				addBook();
-				
-			}
-			else if (num == 2) {
-				deleteBook();
-				
-			}
-			else if (num == 3) {
-				editBook();
-				
-			}
-			else if (num == 4) {
-				viewBook();
-				
-			}
-			else {
-				continue;
-			}
-		}
-	}
+    public static void main(String[] args) {
+        int num = 5;
+        Scanner input = new Scanner(System.in);
+        
+        while (num != 6) {
+            System.out.println("1. Checked-out Books");
+            System.out.println("2. Returned Books");
+            System.out.println("3. Edit Book Number");
+            System.out.println("4. View All Books");
+            System.out.println("5. Show a Menu");
+            System.out.println("6. Exit");
+            System.out.println("Select one number between 1-6: "); 
+            num = input.nextInt();
+
+            if (num == 1) {
+                checkedOut();
+            } else if (num == 2) {
+                returned();    
+            } else if (num == 3) {
+                editBooks();    
+            } else if (num == 4) {
+                viewBooks();
+            } else {
+                continue;
+            }
+        }
+    }
+
+    public static void checkedOut() {
+        Scanner input = new Scanner(System.in); 
+        System.out.print("Student name: ");
+        String studentName = input.next();
+        System.out.print("Book title: "); 
+        String bookTitle = input.next(); 
+        System.out.print("Book Id: "); 
+        String bookId = input.next();
+        System.out.print("Lending date: "); 
+        String lendingDate = input.next();
+        System.out.print("Phone number: ");
+        int phoneNumber = input.nextInt();
+    }
+       
+    public static void returned() { 
+        Scanner input = new Scanner(System.in); 
+        System.out.print("Book Id: ");
+        int bookId = input.nextInt(); 
+    }
+       
+    public static void editBooks() {
+        Scanner input = new Scanner(System.in); 
+        System.out.print("Book Id: ");
+        int bookId = input.nextInt(); 
+    }
+       
+    public static void viewBooks() {
+        Scanner input = new Scanner(System.in); 
+        System.out.print("Student ID: ");
+        int studentId = input.nextInt(); 
+    }
+}
 
 	
-	private static void viewBook() {
-
-		
-	}
 
 
-	private static void editBook() {
 
-		
-	}
-
-
-	private static void deleteBook() {
-
-		
-	}
-
-
-	public static void addBook() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Book ID:");
-		 int BookId = input.nextInt();
-		System.out.print("Book name:");
-		String BooktName = input.next();
-		System.out.print("Email address:");
-		String BookEmail = input.next();
-		System.out.println(BookEmail);
-		System.out.print("Phone number:");
-		String BookPhone = input.next();
-	}
-	
-	public static void deleteBook1() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Book ID:");
-		 int BookId = input.nextInt();
-	}
-	
-	public static void editBook1() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Book ID:");
-		 int BookId = input.nextInt();
-
-	}
-
-	public static void viewBook1() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Book ID:");
-		 int BookId = input.nextInt();
-
-	}
-	
-}	
